9d99403e14c5c2f3ae3f72d7055e75808d284a95
Jose Victor <josevictorferreira@outlook.com>
2022-04-25 21:35:51 -0300

    submitted rust solution

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,17 +3,19 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    let new_speed = speed as f64;
-
-    if new_speed < 4f64 {
-        new_speed * 221f64
-    } else if new_speed < 9f64 {
-        (new_speed * 221f64) * 1.9f64
+    if speed < 5 {
+        (speed as f64) * 221.00
+    } else if speed < 9 {
+        ((speed as f64) * 221.00) * 0.90
     } else {
-        (new_speed * 221f64) * 1.77f64
+        ((speed as f64) * 221.00) * 0.77
     }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    production_items_per_hour(speed)
+    let value:f64 = production_rate_per_hour(speed);
+
+    let result:f64 = value / 60.00;
+
+    (result as u32)
 }
